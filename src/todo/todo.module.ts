83115f10a8546503e73ceb712334aae7ef0178e1
Shahghasi <adil.shahghasi@gmail.com>
2025-03-14 17:32:08 +0430

    wip

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
-import { Todo, TodoSchema } from './schemas/todo.schema';
+import { TodoSchema } from './schemas/todo.schema';
 import { TodoRepository } from './repositories/todo.repository';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }]),
-  ],
+  imports: [MongooseModule.forFeature([{ name: 'Todo', schema: TodoSchema }])],
   controllers: [TodoController],
   providers: [TodoService, TodoRepository],
 })
