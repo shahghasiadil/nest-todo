4f166d8729ced7ef37920e9f3bbc0670d404e7b8
Shahghasi <adil.shahghasi@gmail.com>
2025-03-14 17:31:23 +0430

    wip

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
-
   await app.listen(port);
   console.log(`Application is running on port ${port}`);
 }
