a0a5219bf1cfe8e8d5e3f61c6d826436d12b4f1b
Shahghasi <adil.shahghasi@gmail.com>
2025-03-14 21:13:53 +0430

    wip

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   console.log(`Application is running on port ${port}`);
 }
 bootstrap();
