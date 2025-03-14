29bad94e525c8c429960e6e2034bff040401a873
Shahghasi <adil.shahghasi@gmail.com>
2025-03-14 15:09:50 +0430

    wip

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/todo_db'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
